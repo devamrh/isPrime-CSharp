32386dc0ef8404976cb42880e21465336bd39caa
A. M. Rafinul Huq <116501394+devamrh@users.noreply.github.com>
2023-09-19 21:40:17 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HelloWorld
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Prime Numbers From 1 to 100 :");
+
+
+            for (int i = 2; i <=100; i++)
+
+            {
+
+                bool isPrime = true;
+                for (int j = 2; j*j <=i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        isPrime = false;
+                        break;
+
+                    }
+
+                   
+                }
+
+                if (isPrime)
+                {
+
+                    Console.WriteLine($"{i} ");
+                }
+            }
+        }
+    }
+
+
+
+}
